df42ce1bfd10c0d59613c53bec93e031104c3e6f
Dipu Singh <dsingh197@gmail.com>
2023-09-19 12:53:40 +0530

    update

--- src/components/MasterData.js
+++ src/components/MasterData.js
@@ -1,10 +1,10 @@
 const frontendUrl = "http://127.0.0.1:5173"; // Localhost
 
-const baseAPIUrl = "http://192.168.0.122:8001";
+const baseAPIUrl = "http://192.168.0.122:888";
 
 
 exports.MasterData = {
   baseAPIUrl: baseAPIUrl,
   frontendUrl: frontendUrl,
-  serverPort: 8001
+  serverPort: 888
 };
