b8322e5ad97f4f8bf02ac0029c5b0bbba3211b76
Dipu Singh <dsingh197@gmail.com>
2023-09-19 12:50:43 +0530

    update

--- src/components/middleware/handleDbError.js
+++ src/components/middleware/handleDbError.js
@@ -1,15 +1,23 @@
 const getConnection = require("../../database");
 
-const handleDatabaseError = (req, res, next) => {
-    getConnection()
-      .then((connection) => {
-        connection.release(); // Release the connection back to the pool
-        next();
-      })
-      .catch((error) => {
-        console.error('Database connection error:', error);
-        res.status(500).json({status:false, message: 'Database Connection Failed', data:error });
-      });
-  };
+const test = {
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASS,
+  database: process.env.DB,
+  port: process.env.DBPORT
+}
 
-  module.exports = handleDatabaseError;
+const handleDatabaseError = (req, res, next) => {
+  getConnection()
+    .then((connection) => {
+      connection.release(); // Release the connection back to the pool
+      next();
+    })
+    .catch((error) => {
+      console.error('Database connection error:', error);
+      res.status(500).json({ status: false, message: 'Database Connection Failed', data: error, test: test });
+    });
+};
+
+module.exports = handleDatabaseError;
