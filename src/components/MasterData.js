a468c89ff73c9b67d4fc69569e486bea19684a87
Dipu Singh <dsingh197@gmail.com>
2023-09-20 18:36:56 +0530

    update

--- src/components/MasterData.js
+++ src/components/MasterData.js
@@ -1,10 +1,11 @@
 const frontendUrl = "http://127.0.0.1:5173"; // Localhost
 
-const baseAPIUrl = "http://192.168.0.122:8001";
+// const baseAPIUrl = "http://192.168.0.122:8001"; // Local DMS
+const baseAPIUrl = "http://192.168.0.92:888"; // Live DMS
 
 
 exports.MasterData = {
   baseAPIUrl: baseAPIUrl,
   frontendUrl: frontendUrl,
-  serverPort: 8001
+  serverPort: 888
 };
