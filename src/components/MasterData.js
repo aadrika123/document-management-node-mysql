0e11f4f11c8fc45c7cfb332a300a9ef9618d415f
mrinal9911 <mrinalk123456@gmail.com>
2024-02-17 14:01:03 +0530

    changes

--- src/components/MasterData.js
+++ src/components/MasterData.js
@@ -1,14 +1,11 @@
 const frontendUrl = "http://127.0.0.1:5173"; // Localhost
 
-<<<<<<< HEAD
-const baseAPIUrl = "http://203.129.217.246:888";
-=======
+// const baseAPIUrl = "http://203.129.217.246:888";
 // const baseAPIUrl = "http://192.168.0.122:8001"; // Local DMS
 // const baseAPIUrl = "http://192.168.0.92:888"; // Live DMS
-const baseAPIUrl = "http://172.18.1.50:888"; // Live DMS 
+const baseAPIUrl = "http://172.18.1.50:888"; // Live DMS ok
 
 // const baseAPIUrl = "http://192.168.0.137:888"; // Local DMS
->>>>>>> origin/master
 
 
 exports.MasterData = {
