9933d365df8f0f465798c4ea20b45d5582288754
mrinal9911 <mrinalk123456@gmail.com>
2024-02-18 15:31:48 +0530

    live

--- src/components/MasterData.js
+++ src/components/MasterData.js
@@ -3,8 +3,9 @@ const frontendUrl = "http://127.0.0.1:5173"; // Localhost
 // const baseAPIUrl = "http://203.129.217.246:888";
 // const baseAPIUrl = "http://192.168.0.122:8001"; // Local DMS
 // const baseAPIUrl = "http://192.168.0.92:888"; // Live DMS
-const baseAPIUrl = "http://172.18.1.50:888"; // Live DMS ok
-// const baseAPIUrl = "http://47.247.63.105:888"; // Live DMS ok
+// const baseAPIUrl = "http://172.18.1.50:888"; // Local Public IP DMS ok
+// const baseAPIUrl = "http://47.247.63.105:888"; //Live Public IP DMS ok
+const baseAPIUrl = "https://jharkhandegovernance.com/dms"; //https Live Url IP DMS ok
 
 // const baseAPIUrl = "http://192.168.0.137:888"; // Local DMS
 
