108eab36a13a209ca98a0a09b6ebc81be0bebcd2
kaushal kant <kaushalkantmishra127@gmail.com>
2025-12-23 18:03:45 +0530

    added chhnge in the master data

--- src/components/MasterData.js
+++ src/components/MasterData.js
@@ -6,9 +6,9 @@ const frontendUrl = "http://127.0.0.1:5173"; // Localhost
 // const baseAPIUrl = "http://172.18.1.50:888"; // Local Public IP DMS ok
 // const baseAPIUrl = "http://47.247.63.105:888"; //Live Public IP DMS ok
 
-// const baseAPIUrl = "https://jharkhandegovernance.com/dms"; //https Live Url IP DMS ok
+const baseAPIUrl = "https://jharkhandegovernance.com/dms"; //https Live Url IP DMS ok
 // const baseAPIUrl = "https://aadrikainfomedia.com/dms";    //https Staging Url IP DMS ok
-const baseAPIUrl = "https://egov.rsccl.in/dms";    //https Staging Url IP DMS ok
+// const baseAPIUrl = "https://egov.rsccl.in/dms";    //https Staging Url IP DMS ok
 
 // const baseAPIUrl = "http://192.168.0.137:888"; // Local DMS
 
